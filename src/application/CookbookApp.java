35c9b0837920e3168de7230356f8d1052a8dc961
Aiden Wesley <116656217+wesleyatwit@users.noreply.github.com>
2023-12-06 16:20:41 -0500

    Modified the CookbookApp.java file to rearrange the HBOX

--- src/application/CookbookApp.java
+++ src/application/CookbookApp.java
@@ -1,12 +1,15 @@
 package application;
 
 import javafx.application.Application;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
-import javafx.scene.layout.VBox;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
 import java.util.ArrayList;
@@ -20,6 +23,7 @@ public class CookbookApp extends Application {
     private List<Dish> dislikedDishes;
 
     private ImageView dishImageView;
+    private Text dishInfoText;
     private Button likeButton;
     private Button dislikeButton;
 
@@ -37,6 +41,12 @@ public class CookbookApp extends Application {
         dislikedDishes = new ArrayList<>();
 
         dishImageView = new ImageView();
+        dishImageView.setFitWidth(667);
+        dishImageView.setFitHeight(400);
+
+        dishInfoText = new Text();
+        dishInfoText.setWrappingWidth(400);
+
         likeButton = new Button("Like");
         dislikeButton = new Button("Dislike");
 
@@ -47,8 +57,16 @@ public class CookbookApp extends Application {
         likeButton.setOnAction(e -> handleSwipe(true));
         dislikeButton.setOnAction(e -> handleSwipe(false));
 
-        VBox root = new VBox(10, dishImageView, likeButton, dislikeButton);
-        Scene scene = new Scene(root, 400, 400);
+        HBox buttonsBox = new HBox(10, likeButton, dislikeButton);
+        buttonsBox.setAlignment(Pos.CENTER);
+
+        BorderPane root = new BorderPane();
+        root.setTop(dishImageView);
+        BorderPane.setAlignment(dishImageView, Pos.CENTER); // Center the image
+        root.setCenter(dishInfoText);
+        root.setBottom(buttonsBox);
+
+        Scene scene = new Scene(root, 400, 600);
 
         primaryStage.setTitle("Cookbook App");
         primaryStage.setScene(scene);
@@ -59,12 +77,22 @@ public class CookbookApp extends Application {
     }
 
     private void showRandomDish() {
-        Random random = new Random();
-        int randomIndex = random.nextInt(dishes.size());
-        Dish currentDish = dishes.get(randomIndex);
+        if (!dishes.isEmpty()) {
+            Random random = new Random();
+            int randomIndex = random.nextInt(dishes.size());
+            Dish currentDish = dishes.get(randomIndex);
 
-        Image image = new Image(currentDish.getImagePath());
-        dishImageView.setImage(image);
+            Image image = new Image(currentDish.getImagePath());
+            dishImageView.setImage(image);
+
+            // Set the food information to the text
+            dishInfoText.setText("Food: " + currentDish.getName() + "\nOrigin: " + currentDish.getCuisineType()
+                    + "\nDescription: " + currentDish.getDescription() + "\nCooktime: " + currentDish.getCookTime()
+                    + " mins" + "\nRecipe:\n" + currentDish.getRecipe());
+
+            // Clear any previous liked or disliked information
+            dishInfoText.setText(dishInfoText.getText() + "\n\n");
+        }
     }
 
     private void handleSwipe(boolean liked) {
@@ -97,10 +125,10 @@ public class CookbookApp extends Application {
 
         // Determine the swipe direction based on the change in X position
         double deltaX = xFinal - xInitial;
-        if (deltaX > 30) {
+        if (deltaX > 50) {
             // Right swipe, consider it as "Like"
             handleSwipe(true);
-        } else if (deltaX < -30) {
+        } else if (deltaX < -50) {
             // Left swipe, consider it as "Dislike"
             handleSwipe(false);
         }
